284c23d5621bcab854ba82ec050cc6cd5799d2f6
mfaizan031 <135124209+mfaizan031@users.noreply.github.com>
2025-08-07 11:41:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 # from src.models.user import db # Removed database import
-from src.routes.user import user_bp
 from src.routes.drive_api import drive_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
